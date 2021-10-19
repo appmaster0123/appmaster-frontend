64c6529403e8037071cdd98a6190b7904fbea20c
appmaster0123 <appmaster0123@gmail.com>
2021-10-19 10:50:19 -0300

    commit inicial

---
+++ screens/Login.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from '../style/MainStyle';
+
+
+export default function Login({navigation}) {
+
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    const entrar = () => {    
+        navigation.reset({
+            index: 0,
+            routes: [{name: "Principal"}]
+        })
+    }
+
+    const cadastrar = () => {
+        navigation.navigate("Cadastro")
+    }
+
+  
+
+    return (
+        <View style={[styles.container, specificStyle.specificContainer]}> 
+        
+            <Text h3>Entre no TemTudaki</Text>
+            <Input
+                placeholder="E-mail"
+                leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                onChangeText={value => setEmail(value)}
+                keyboardType="email-address"
+            />
+            
+            <Input
+                placeholder="Sua senha"
+                leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                onChangeText={value => setPassword(value)}
+                secureTextEntry={true}
+            />
+        
+            <Button
+                icon={
+                <Icon
+                    name="check"
+                    size={15}
+                    color="white"
+                />
+                }
+                title="Entrar"
+                buttonStyle={specificStyle.button}
+                onPress={() => entrar()}
+            />
+          
+            <Button
+                icon={
+                <Icon
+                    name="user"
+                    size={15}
+                    color="white"
+                />
+                }
+                title=" Cadastrar"
+                buttonStyle={specificStyle.button}
+                onPress={() => cadastrar()}
+            />
+    </View>
+  );
+}
+
+const specificStyle = StyleSheet.create({
+  specificContainer: {
+    backgroundColor: "#fff"
+  },
+  button: {
+    width: "100%",
+    marginTop: 10
+  }
+})
